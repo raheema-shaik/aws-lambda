7f67a2b25cac683c12c5f6a8e00111485774158c
Shaik Raheema <163520578+raheema-shaik@users.noreply.github.com>
2024-05-01 18:43:25 +0530

    sqs configuration changed

---
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -0,0 +1,29 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RegionScope;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "sns_handler",
+	roleName = "sns_handler-role",
+	isPublishVersion = false,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SnsEventSource(regionScope = RegionScope.DEFAULT, targetTopic = "lambda_topic")
+public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("body", "Hello from Lambda");
+		return resultMap;
+	}
+}
